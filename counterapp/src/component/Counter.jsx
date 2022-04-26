e348771dc103baf93413a83133153144921b2e61
arai5563 <91533123+arai5563@users.noreply.github.com>
2022-04-27 03:48:53 +0530

    commit

--- counterapp/src/component/Counter.jsx
+++ counterapp/src/component/Counter.jsx
@@ -3,38 +3,54 @@ import "./Counter.css";
 
 const Counter = () => {
 
-    let [start,setStart] = useState(10);
+  let [start, setStart] = useState(1);
 
-    const decreament = () => {
+  const decreament = () => {
 
-         if(start > 0){
+    if (start > 0) {
 
-             setStart(start -1)
-         }else{
-             setStart(0)
-         }
+      setStart(start - 1)
     }
-    const increament =()=> {
-
-      
-       setStart(start + 1)
+  }
+  const increament = (e) => {
+    if (start == 999) {
+      alert("reached Max value")
     }
+    if ((start >= 0) && (start < 1000)) {
+      setStart(start + 1)
+    }
+  }
+
+  const handleChange = (e) => {
+
+    setStart("");
+
+    if (start < 1000) {
+      start = e.target.value;
+      setStart(+start)
+    }
+
+    if (start >= 1000) {
+      alert("Entered value is greater than Max Value")
+    }
+
+  }
 
   return (
     <>
-    
-    <div className='main'>
-      
+
+      <div className='main'>
+
         <div className='container'>
 
-        <div className='decr'><button onClick={decreament}>-</button></div>
-        <div className='inp'><input type="number"  value={start} /></div>
-        <div className='inc'><button onClick={increament}>+</button></div>
+          <div className='decr'><button onClick={decreament}>-</button></div>
+          <div className='inp'><input type="numeric" value={start} onChange={handleChange} /></div>
+          <div className='inc'><button onClick={increament}>+</button></div>
 
         </div>
 
-    </div>
-    
+      </div>
+
     </>
   )
 }
